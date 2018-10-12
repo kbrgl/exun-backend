f82899a622f5804130a6db152a71054222381e7b
Kabir Goel <kabirgoel.kg@gmail.com>
2018-10-13 03:04:09 +0530

    Add new admin

---
+++ public/admin/precache-manifest.b64bbc5aa42aeab26eeeabf9d87ddd65.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "3f82f5bf8566dc34c9b5",
+    "url": "/admin/static/js/runtime~main.3f82f5bf.js"
+  },
+  {
+    "revision": "1c12b0f0e2f4d6255c27",
+    "url": "/admin/static/js/main.1c12b0f0.chunk.js"
+  },
+  {
+    "revision": "d8874792fe3f7f18b437",
+    "url": "/admin/static/js/1.d8874792.chunk.js"
+  },
+  {
+    "revision": "1c12b0f0e2f4d6255c27",
+    "url": "/admin/static/css/main.4af9227d.chunk.css"
+  },
+  {
+    "revision": "f12de7a643be50dd0d94f66acc66ed96",
+    "url": "/admin/index.html"
+  }
+];
